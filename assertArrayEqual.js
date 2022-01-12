376112abcee7861a46f34e0aa31886140ab49404
Victoria Matsushita <vmatsushita45@gmail.com>
2022-01-12 07:22:11 -0800

    code for assertArray Equals added

---
+++ assertArrayEqual.js
@@ -0,0 +1,19 @@
+const assertArray = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length)
+    return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+assertArray([1, 2, 3], [1, 2, 4]);
