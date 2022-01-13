fbaeb911e021fe5efb795616ba625d08e466340b
Victoria Matsushita <vmatsushita45@gmail.com>
2022-01-12 23:15:10 -0800

    function for countOnly

---
+++ countOnly.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const countOnly = function(allItems, itemsToCount) {
+  let result = {};
+  for (let item of allItems) {
+    if (itemsToCount[item]) {
+      if (result[item]) {
+        result[item]++;
+      } else {
+        result[item] = 1;
+      }
+    }
+  }
+  return result;
+};
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true, "Agouhanna": false});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
